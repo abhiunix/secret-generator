e58f157cdfaa585176f8486f2b1c50c9b78f2af9
Abhijeet Singh <abhi009215@gmail.com>
2020-09-10 18:04:35 +0530

    Adding Bannergit add .

--- secrets_generator.py
+++ secrets_generator.py
@@ -1,7 +1,20 @@
+#!/usr/bin/python3.7
+
 import secrets
 import string
 import sys
+import os
+from colorama import Fore,Style
+
+
+def banner():
+	os.system("clear")
+	os.system("pyfiglet 'Secret Generator'")
+	print( Style.BRIGHT + Fore.WHITE + """                                                    
+             """ + Style.DIM + Fore.BLUE + "\t\t@abhiunix\n\n")
+banner()
 
 Secretkey = ""
 for i in range(int(str(sys.argv[1]))):
-	print(Secretkey.join(secrets.choice(string.digits + string.ascii_letters + string.punctuation)), end='')
+	secretX= print(Secretkey.join(secrets.choice(string.digits + string.ascii_letters + string.punctuation)), end='')
+print("Secret is: " + str(secretX))
